40b6c1b49d75911a801464e502d4a74796e6f8a7
Kristijan Turić <kristijan.turic1@gmail.com>
2025-02-11 20:19:23 +0100

    Add Resume

---
+++ src/components/Resume.jsx
@@ -0,0 +1,12 @@
+import "../styles/Resume.css";
+import PersonalDetailsInfo from "./PersonalDetailsInfo";
+
+function Resume() {
+    return (
+        <div className="resume-container">
+            <PersonalDetailsInfo />
+        </div>
+    );
+}
+
+export default Resume;
