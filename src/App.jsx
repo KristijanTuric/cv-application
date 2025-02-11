a3f77ff45d698fd34d8042dce44dafdd880079b4
Kristijan Turić <kristijan.turic1@gmail.com>
2025-02-11 20:19:46 +0100

    Add App and update core style

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './styles/App.css'
+import PersonalDetails from './components/PersonalDetails';
+import Resume from './components/Resume';
+
+function App() {
+
+  return (
+    <div className='app'>
+      <PersonalDetails  />
+      <Resume />
+    </div>
+  );
+}
+
+export default App
