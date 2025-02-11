65152de9f0ecd31bbcc052063c45428a37c38de7
Kristijan Turić <kristijan.turic1@gmail.com>
2025-02-11 20:19:03 +0100

    Add Personal Details Info component

---
+++ src/components/PersonalDetailsInfo.jsx
@@ -0,0 +1,16 @@
+import "../styles/PersonalDetailsInfo.css";
+
+function PersonalDetailsInfo() {
+    return (
+        <div className="personal-details-container">
+            <h1>Kristijan Turić</h1>
+            <div className="contact-details">
+                <h3>kristijan.turic@gmail.com</h3>
+                <h3>+66 1857 123</h3>
+                <h3>Berlin, DE</h3>
+            </div>
+        </div>
+    );
+}
+
+export default PersonalDetailsInfo;
