f4b71181be3d0e483462121940a8a809d60ca3c7
Kristijan Turić <kristijan.turic1@gmail.com>
2025-03-11 16:09:53 +0100

    Add icons to personal info

--- src/components/PersonalDetailsInfo.jsx
+++ src/components/PersonalDetailsInfo.jsx
@@ -1,13 +1,26 @@
 import "../styles/PersonalDetailsInfo.css";
+import Icon from "@mdi/react";
+import { mdiEmail, mdiPhone, mdiHome } from "@mdi/js";
 
 function PersonalDetailsInfo({ details }) {
     return (
         <div className="personal-details-container">
             <h1>{details.fullName || "John Doe"}</h1>
             <div className="contact-details">
-                <h3>{details.email || "john@email.com"}</h3>
-                <h3>{details.phone || "+66 1234 123"}</h3>
-                <h3>{details.address || "Berlin, DE"}</h3>
+                <div className="contact-element">
+                    <Icon path={mdiEmail} size={1}/>
+                    <h3>{details.email || "john@email.com"}</h3>
+                </div>
+                
+                <div className="contact-element">
+                    <Icon path={mdiPhone} size={1}/>
+                    <h3>{details.phone || "+66 1234 123"}</h3>
+                </div>
+
+                <div className="contact-element">
+                    <Icon path={mdiHome} size={1}/>
+                    <h3>{details.address || "Berlin, DE"}</h3>
+                </div>
             </div>
         </div>
     );
