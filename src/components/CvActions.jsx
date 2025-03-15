687ea49f1f1231949e7ce1534cdcb450acb44dc2
Kristijan Turić <kristijan.turic1@gmail.com>
2025-03-15 21:14:06 +0100

    Update cv example

--- src/components/CvActions.jsx
+++ src/components/CvActions.jsx
@@ -5,7 +5,8 @@ function CvActions({setPersonalDetails, setEducationList, setExperienceList, set
     function addExample() {
         setPersonalDetails({fullName: "Geralt of Rivia", email: "geralt@kaermorhen.com", phone: "+123-456-789", address: "Kaer Morhen, The Continent"});
         setEducationList([{institution: "Kaer Morhen School of the Wolf", degree: "Ph.D in Alchemy & Elixirs", startingYear: "01-01-1265", graduatingYear: "01-01-1269"}]);
-        setExperienceList([{position: "Royal Advisor (Briefly)", workplace: "Nilfgaardian Empire", startingYear: "01-01-1268", endYear: null, jobResponsabilities: ""}]);
+        setExperienceList([{position: "Royal Advisor (Briefly)", workplace: "Nilfgaardian Empire", startingYear: "01-01-1268", endYear: "", 
+            jobResponsabilities: ["Provided counsel on supernatural threats", "Assisted in the search for Ciri, ensuring the safety of the Elder Blood"]}]);
         setSummary("Experienced monster slayer and expert in supernatural threats, specializing in contract-based monster hunting, alchemy, and swordsmanship. Trained at Kaer Morhen, I have spent decades traversing The Continent, solving curses, and battling creatures of legend. Known for my sharp instincts, even sharper blades, and a strong moral code—when coin is involved.");
     }
 
